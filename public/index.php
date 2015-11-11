1a9104d5f406ad4f6fffac422c739647420266de
Daniel Pereira <danielpereirabp@gmail.com>
2015-11-11 17:22:11 -0200

    Alterações no index.php

--- public/index.php
+++ public/index.php
@@ -3,10 +3,14 @@
 require '../vendor/autoload.php';
 
 $client = new GuzzleHttp\Client();
+
 $res = $client->get('http://httpbin.org/get');
-echo $res->getStatusCode();
+
+echo $res->getStatusCode() . PHP_EOL;
 // "200"
-echo $res->getHeader('content-type');
+
+echo $res->getHeader('content-type') . PHP_EOL;
 // 'application/json; charset=utf8'
-echo $res->getBody();
+
+echo $res->getBody() . PHP_EOL;
 // {"type":"User"...
