7baa3520a0ab921f3c672ad018a301cda2e1e35f
Daniel Pereira <danielpereirabp@gmail.com>
2015-11-11 10:10:11 -0200

    Feito uma requisição http com o Guzzle

--- public/index.php
+++ public/index.php
@@ -1,3 +1,14 @@
 <?php
 
-echo 'Hello World!';
+require 'vendor/autoload.php';
+
+$client = new GuzzleHttp\Client();
+$res = $client->request('GET', 'https://api.github.com/user', [
+    'auth' => ['user', 'pass']
+]);
+echo $res->getStatusCode();
+// "200"
+echo $res->getHeader('content-type');
+// 'application/json; charset=utf8'
+echo $res->getBody();
+// {"type":"User"...
